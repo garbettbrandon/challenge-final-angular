9b25faec0f9d4e2c9919e1550cd122fe81c65318
garbettbrandon <garbettbrandon@gmail.com>
2025-03-21 12:37:56 +0100

    feat: implement podcast search functionality and enhance home page layout with dynamic results counter

--- src/app/core/services/podcast.service.ts
+++ src/app/core/services/podcast.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { ItunesResponse } from '../interfaces/podcast.interface';
+import { map, Observable } from 'rxjs';
+import { Entry, ItunesResponse } from '../interfaces/podcast.interface';
 import { environment } from '../../../environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -11,10 +11,24 @@ export class PodcastService {
   private http = inject(HttpClient);
 
   getTopPodcasts(): Observable<ItunesResponse> {
+    return this.http.get<ItunesResponse>(
+      `${baseUrl}/us/rss/toppodcasts/limit=100/genre=1310/json`
+    );
+  }
+
+  searchPodcast(query: string): Observable<Entry[]> {
+    query = query.toLowerCase();
+
     return this.http
       .get<ItunesResponse>(
         `${baseUrl}/us/rss/toppodcasts/limit=100/genre=1310/json`
       )
-      .pipe(tap(console.log));
+      .pipe(
+        map(({ feed }) =>
+          feed.entry.filter((entry) =>
+            entry['im:name'].label.toLowerCase().includes(query)
+          )
+        )
+      );
   }
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { PodcastCardComponent } from '../../core/components/podcast-card/podcast-card.component';
 import { PodcastService } from '../../core/services/podcast.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -11,9 +12,18 @@ import { rxResource } from '@angular/core/rxjs-interop';
 })
 export class HomePageComponent {
   podcastService = inject(PodcastService);
+  query = signal('');
 
   podcastResource = rxResource({
     request: () => ({}),
     loader: ({ request }) => this.podcastService.getTopPodcasts(),
+  });
+
+  searchResource = rxResource({
+    request: () => ({ query: this.query() }),
+    loader: ({ request }) => {
+      if (!request.query) return of([]);
+      return this.podcastService.searchPodcast(request.query);
+    },
   });
 }
